3c394f09aedd42509f3a3742f01ae8206379e101
nirmalwsolus <nirmal@wsolus.com>
2017-05-11 11:23:02 +0530

    Add connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -2,18 +2,33 @@
  * Created by nirmal on 5/11/17.
  */
 const path= require('path');
-const publicPath= path.join(__dirname,'../public');
-
+const http= require('http');
 const express=require('express');
+const scoketIO=require('socket.io');
 
-var app=express();
-
+const publicPath= path.join(__dirname,'../public');
 //heroku port confgurations
 const port=process.env.PORT || 3000;
+var app=express();
+
+//now we use http server instead express
+var server=http.createServer(app);
+
+//web socket server
+var io=scoketIO(server);
+
 
 //middleware
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+    socket.on('disconnect',()=>{
+        console.log('User was disconnected')
+    });
+});
+
+server.listen(port,()=>{
     console.log(`Started on port ${port}`);
 });
