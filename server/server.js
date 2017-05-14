e1941c23c95059b135b46eda7994095ae173a789
nirmalwsolus <nirmal@wsolus.com>
2017-05-14 20:58:08 +0530

    Adding user chat groups

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -12,12 +13,32 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    //
+    // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.on('join', (params,callback) => {
+        if(!isRealString(params.name) || !isRealString(params.room)){
+            //runs the err callback because function has a parameter with the callback function
+            callback('Name and room name are requires');
+        }else {
+            //join the group
+            socket.join(params.room);
+
+            //io.emit - to all io.to('The office Fans').emit-to all the people in that room
+            //socket.broadcast.emit - to all except current socket.broadcast.to('The office Fans).emit
+            //socket.emit - specifically to one user
+
+            socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+            socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+            callback();
+        }
+    });
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
